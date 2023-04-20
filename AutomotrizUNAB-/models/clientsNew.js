bd264b313b5713d4599ff478e329685b2ec8de3c
HumbeiroFajardomt <humbeiro.fajardo.mt@usa.edu.co>
2023-04-20 11:57:01 -0500

    new Models file

---
+++ AutomotrizUNAB-/models/clientsNew.js
@@ -0,0 +1,13 @@
+// import mongoose from "mongoose";
+
+// const mantenimiento = ["Proceso", "Reparado", "Instalado", "Inspeccion"];
+
+// const componentsModel = mongoose.Schema({
+//   id_component: { type: Number, required: true },
+//   name: { type: String, maxlength: 30, minlength: 3, required: true },
+//   brand: { type: String, maxlength: 30, minlength: 3, required: true },
+//   year: { type: String, required: true },
+//   description: { type: String, required: true, enum: mantenimiento },
+// });
+// export default mongoose.model("component", componentsModel);
+//  NoMofication establsihed
